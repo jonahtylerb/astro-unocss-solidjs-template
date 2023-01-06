bb50b777a1f139cf647beef1ac0470365776079f
Jonah Brupbacher <jonahtylerb@gmail.com>
2023-01-05 20:31:50 -0600

    Fixed some things that I forgot

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,7 +4,8 @@ const Counter: Component = () => {
   const [count, setCount] = createSignal(0);
   return (
     <button
-      class='-translate-1/2 left-20 top-20 absolute z-10 p-5 rounded-xl bg-zinc-7 shadow-xl text-2xl active:bg-zinc-9 active:shadow-none duration-100'
+      type='button'
+      class='left-10 top-10 absolute z-10 p-5 rounded-xl bg-zinc-7 shadow-xl text-2xl active:bg-zinc-9 active:shadow-none duration-100'
       onClick={() => {
         setCount((count) => count + 1);
       }}
