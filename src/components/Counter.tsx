e9466f6824a865b65912be93d3e99c6dae55e575
jonahtylerb <Jonahtylerb@gmail.com>
2023-01-17 16:08:29 -0600

    Small change in tasks

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -8,8 +8,7 @@ const Counter: Component = () => {
       class='left-10 top-10 absolute z-10 p-5 rounded-xl bg-zinc-7 shadow-xl text-2xl active:bg-zinc-9 active:shadow-none duration-100'
       onClick={() => {
         setCount((count) => count + 1);
-      }}
-    >
+      }}>
       {count()}
     </button>
   );
